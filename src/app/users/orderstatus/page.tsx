9418701d8f31411a8d901207917f2e6a742cf954
antonelli-dev <antonelli.developer@gmail.com>
2024-09-25 16:28:30 +0200

    cleaned order status page code

--- src/app/users/orderstatus/page.tsx
+++ src/app/users/orderstatus/page.tsx
@@ -1,17 +1,5 @@
-import { Truck, Package, CheckCircle } from 'lucide-react'
-import { GetOrdersUseCase } from '@/core/application/usecases/get-orders.use-case';
-
-// async function getOrders() {
-//   await new Promise(resolve => setTimeout(resolve, 1000))
-  
-//   return [
-//     { id: '1', product: 'Smartphone', status: 'shipped', date: '2023-05-15' },
-//     { id: '2', product: 'Laptop', status: 'processing', date: '2023-05-16' },
-//     { id: '3', product: 'Headphones', status: 'delivered', date: '2023-05-14' },
-//     { id: '4', product: 'Smart Watch', status: 'processing', date: '2023-05-17' },
-//     { id: '5', product: 'Tablet', status: 'shipped', date: '2023-05-15' },
-//   ]
-// }
+import { Truck, Package, CheckCircle } from "lucide-react";
+import { GetOrdersUseCase } from "@/core/application/usecases/get-orders.use-case";
 
 export default async function OrderStatusList() {
   const orders = await new GetOrdersUseCase().execute();
@@ -27,30 +15,46 @@ export default async function OrderStatusList() {
                 <div className="px-4 py-4 sm:px-6">
                   <div className="flex items-center justify-between">
                     <p className="text-sm font-medium text-indigo-600 truncate">
-                      Order number: 
-                      {order.order_id.toString().padStart(4, '0')}
+                      Order number:
+                      {order.order_id.toString().padStart(4, "0")}
                     </p>
                     <div className="ml-2 flex-shrink-0 flex">
-                      {order.status == 'Processing' && <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-gray-800">{order.status}</span>}
-                      {order.status == 'Shipped' && <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">{order.status}</span>}
-                      {order.status == 'Delivered' && <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">{order.status}</span>}
+                      {order.status == "Processing" && (
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-gray-800">
+                          {order.status}
+                        </span>
+                      )}
+                      {order.status == "Shipped" && (
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
+                          {order.status}
+                        </span>
+                      )}
+                      {order.status == "Delivered" && (
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                          {order.status}
+                        </span>
+                      )}
                     </div>
                   </div>
                   <div className="mt-2 sm:flex sm:justify-between">
                     <div className="sm:flex">
                       <p className="flex items-center text-sm text-gray-500">
-                        {order.status === 'Processing' && <Package className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" />}
-                        {order.status === 'Shipped' && <Truck className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" />}
-                        {order.status === 'Delivered' && <CheckCircle className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" />}
-                        {order.status === 'Processing' && 'Processing'}
-                        {order.status === 'Shipped' && 'Shipped'}
-                        {order.status === 'Delivered' && 'Delivered'}
+                        {order.status === "Processing" && (
+                          <Package className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" />
+                        )}
+                        {order.status === "Shipped" && (
+                          <Truck className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" />
+                        )}
+                        {order.status === "Delivered" && (
+                          <CheckCircle className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" />
+                        )}
+                        {order.status === "Processing" && "Processing"}
+                        {order.status === "Shipped" && "Shipped"}
+                        {order.status === "Delivered" && "Delivered"}
                       </p>
                     </div>
                     <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                      <p>
-                        Fecha de orden: 21-11-2024
-                      </p>
+                      <p>Fecha de orden: 21-11-2024</p>
                     </div>
                   </div>
                 </div>
@@ -60,5 +64,5 @@ export default async function OrderStatusList() {
         </div>
       </div>
     </div>
-  )
+  );
 }
